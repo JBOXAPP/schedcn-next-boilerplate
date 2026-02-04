508ce5f4975005f0c08402fb85f8ff49f980ec7f
SauravYmir <saurav@ymirlabs.com>
2026-02-04 14:28:01 +0530

    chore: update config files and add dockerignore
    - Update tsconfig.json to exclude jbox directories
    - Configure Next.js to allow images from unsplash.com
    - Add .dockerignore file to exclude unnecessary files from Docker builds
    - Update ESLint rules to warn on unused variables and enforce img tags

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@next/next/no-img-element": "error",
+      "react/no-unescaped-entities": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
